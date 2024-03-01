3ba414a33a02c788929c255e3497e1e7c0538d39
wanghao <1059240062@qq.com>
2024-03-01 13:32:34 +0800

    图片资源推送

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,7 @@
 const {defineConfig} = require('@vue/cli-service')
 module.exports = defineConfig({
+    publicPath: process.env.NODE_ENV === 'production'
+        ? '/english/'  // 在发布到 GitHub Pages 时，调整成你的项目子目录名
+        : '/',
     transpileDependencies: true
 })
